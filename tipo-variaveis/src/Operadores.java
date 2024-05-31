6f7bc424a0592137df512fd5fb7927c3f994b4a7
euZont3R <euandrepaixao@hotmail.com>
2024-05-31 12:26:04 -0300

    Atualização dos exercicios de Operdadores

--- tipo-variaveis/src/Operadores.java
+++ tipo-variaveis/src/Operadores.java
@@ -1,6 +1,6 @@
 public class Operadores {
     public static void main(String[] args) {
-    String concatenacao = "?";
+    /*String concatenacao = "?";
 
     concatenacao = 1+1+1+"1";
 
@@ -12,6 +12,59 @@ public class Operadores {
 
     concatenacao = 1+"1"+1+"1";
     
-    System.out.println(concatenacao);
+    System.out.println(concatenacao);*/
+
+//-------------------------------------------------
+    
+/*  int numero = 5;
+
+    numero = - numero;
+
+    System.out.println(numero);
+
+    numero = numero * -1;
+
+    System.out.println(numero); */
+
+// --------------------------------------------------
+
+/* int numero = 5;
+
+    //x repetição
+
+    //mumero = numero +1;
+
+    System.out.println( numero --);
+    
+    System.out.println(numero);
+
+    boolean variavel = true;
+    
+    System.out.println(variavel);
+    System.out.println(!variavel);*/
+    
+//--------------------------------------------
+
+/*    int a, b;
+
+    a = 6;
+    b = 6;
+    String resultado = a==b ?"verdadeiro" : "falso";
+
+    System.out.println(resultado); */
+
+    boolean condicao1=true;
+
+    boolean condicao2=true;
+
+    if(condicao1 && condicao2){
+        System.out.println("as duas condições são verdadeiras");
+    }
+
+    if(condicao1 || condicao2){
+        System.out.println("uma das condições é verdadeiras");
+    }
+
+    System.out.println("fim");
     }
 }
--- tipo-variaveis/src/TipoVariaveis.java
+++ tipo-variaveis/src/TipoVariaveis.java
@@ -2,5 +2,7 @@ public class TipoVariaveis {
     public static void main(String[] args) throws Exception {
         double salarioMinino = 1874;
 
+        System.out.println(salarioMinino);
+
     }
 }
