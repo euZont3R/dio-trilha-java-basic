36abd2c9c401d15b4d7eaf0b3b07b8acd63bbbb4
euZont3R <euandrepaixao@hotmail.com>
2024-06-04 23:54:52 -0300

    ssss

--- JAVA-TERMINAL/java-terminal/src/AboutMe.java
+++ JAVA-TERMINAL/java-terminal/src/AboutMe.java
@@ -5,11 +5,9 @@ public class AboutMe {
         String sobrenome = args [1];
         int idade = Integer.valueOf(args[2]);
         double altura = Double.valueOf(args[3]);
-
+    
         System.out.println("Olá, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura é " + altura + "cm");
-        
-        
     }
 }
