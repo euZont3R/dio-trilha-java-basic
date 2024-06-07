c88e0c0b5d4d4b4102a9b6bd4bd167861eeb5ea6
euZont3R <euandrepaixao@hotmail.com>
2024-06-06 21:23:29 -0300

    Arquivos update

---
+++ JAVA-TERMINAL/java-terminal/AboutMe/src/AboutMe.java
@@ -0,0 +1,13 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+    
+    
+    }
+}
