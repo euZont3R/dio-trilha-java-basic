24ca31812cdee753da638bb61b985abd35478769
euZont3R <euandrepaixao@hotmail.com>
2024-06-10 21:55:53 -0300

    Resumo da aula ExemploForArray

--- Nova pasta/controle-fluxo/src/ExemplosForArray.java
+++ Nova pasta/controle-fluxo/src/ExemplosForArray.java
@@ -1,5 +1,13 @@
 public class ExemplosForArray {
     public static void main(String[] args) {
+        String alunos [] = { "FELIPE", "Jonas", "JULIA", "MARCOS" };
+
+        /*  for (int x=0; x<alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);  */ 
         
-    }
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é: " + aluno);
+            }       
+        }
+  
 }
